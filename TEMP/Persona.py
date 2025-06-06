00eb025adf888db0b7f040c9d089c608072b2eef
Javier Ovelar <isaiasjovelar@gmail.com>
2025-06-06 02:34:11 -0300

    Removido contenido redundante

--- TEMP/Persona.py
+++
@@ -1,8 +0,0 @@
-class Persona:
-    def __init__(self,apellido,nombre,dni):
-        self.apellido=apellido
-        self.nombre=nombre
-        self.dni=dni
-    
-    def mostrar(self):
-        print(f"{self.nombre}, {self.dni}")
